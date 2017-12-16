014a4b035d98b94e9c64b8ebab9a780193503dad
koooootake <kooooooooootake@gmail.com>
2017-12-16 15:43:41 +0900

    Initial commit

---
+++ osyaberiya/osyaberiya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  osyaberiya
+//
+//  Created by koooootake on 2017/12/16.
+//  Copyright © 2017年 koooootake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
